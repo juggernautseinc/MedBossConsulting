314e21e22892e409311a1bc36db61b77a68c0d14
Sherwin <sherwingaddis@gmail.com>
2021-11-08 23:46:23 -0500

    Setting database password for clinic install script

--- setup.php
+++ setup.php
@@ -25,7 +25,7 @@ if ($response !== true) {
 }
 
 function randomPassword() {
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-';
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()-';
     $pass = array(); //remember to declare $pass as an array
     $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
     for ($i = 0; $i < 16; $i++) {
