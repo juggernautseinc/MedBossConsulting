5b196785f6401904ceff988e5bad3a45aab952d5
Sherwin <sherwingaddis@gmail.com>
2021-11-26 18:24:41 -0500

    Trying to unlink key files remove unlink  php file

--- library/classes/Installer.class.php
+++ library/classes/Installer.class.php
@@ -1427,7 +1427,7 @@ $config = 1; /////////////
     {
         $dir = dirname(__DIR__, 2);
         file_put_contents("/var/www/html/errors/dir.txt", $dir);
-        $location = $dir ."/boss/sites/". $site_id . "/documents/logs_and_misc/methods/";
+        $location = $dir ."/sites/". $site_id . "/documents/logs_and_misc/methods/";
         unlink($location . "sixa");
         unlink($location . "sixb");
     }
