7f2bb8bb8c47af4f73d1b926e85472202a2f6297
Sherwin <sherwingaddis@gmail.com>
2021-11-26 16:57:05 -0500

    Trying to unlink key files

--- setup.php
+++ setup.php
@@ -1528,8 +1528,7 @@ STP5BOT;
                         echo "<div class='jumbotron p-5'>";
                         echo "<p>Select a theme for OpenEMR...</p>\n";
                         $btn_text = "Proceed to Final Step";
-                        require_once("unlink.php?site=" . $site_id);
-                        //$installer->deleteKeys($site_id);
+                        $installer->deleteKeys($site_id);
                         $installer->displaySelectedThemeDiv();
                         $theme_form = <<<TMF
                         <div class='row'>
