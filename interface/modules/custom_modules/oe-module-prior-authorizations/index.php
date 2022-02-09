11b0dde990fcb3354820360ff151efc80d496702
Sherwin <sherwingaddis@gmail.com>
2022-02-09 13:13:28 -0500

    Build prior authorization module start display here

--- interface/modules/custom_modules/oe-module-prior-authorizations/index.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  *  package OpenEMR
  *  link    https://www.open-emr.org
@@ -7,5 +8,39 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+require_once dirname(__FILE__, 4) . "/globals.php";
 
-echo "landed";
+use OpenEMR\Core\Header;
+
+$pid = $_SESSION['pid']
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo xlt('Prior Authorization Manager'); ?></title>
+    <?php Header::setupHeader(['common'])?>
+</head>
+<body>
+    <div class="container">
+        <div class="m-4">
+            <h1><?php echo xlt('Prior Authorization Manager'); ?></h1>
+        </div>
+        <div class="m-4">
+            <?php if (empty($pid)) {
+                die("You must be in a patients Chart to enter this information");
+            } ?>
+            <form id="theform" method="post" action="index.php" onsubmit="top-restoreSession()">
+                <div class="form-group">
+                    <label for=""
+                </div>
+            </form>
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ interface/modules/custom_modules/oe-module-prior-authorizations/table.sql
@@ -0,0 +1,19 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE TABLE IF NOT EXIST `module_prior_authorizations`
+(
+    `id`  INT NOT NULL PRIMARY KEY auto_increment,
+    `pid` INT NULL,
+    `auth_num` VARCHAR,
+    `start_date` DATE,
+    `end_data` DATE,
+    `cpt` TEXT,
+    `init_units` INT,
+    `remaining_units` INT
+) ENGINE = InnoDB COMMENT = 'Store authorizations';
