f7210fc998aced3c4e604fddcaa8c01e6e521120
Sherwin <sherwingaddis@gmail.com>
2022-02-11 00:38:52 -0500

    Build prior authorization module saving data

--- interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
@@ -27,9 +27,9 @@ if (!empty($_POST['token'])) {
     $postData->setPid($pid);
     $postData->setAuthNum($_POST['authorization']);
     $postData->setInitUnits($_POST['units']);
-    $startDate = oeFormatDateTime($_POST['start_date']);
+    echo $startDate = oeFormatDateTime($_POST['start_date']);
     $postData->setStartDate($startDate);
-    $endDate = oeFormatDateTime($_POST['end_date']);
+    echo $endDate = oeFormatDateTime($_POST['end_date']);
     $postData->setEndDate($endDate);
     $postData->setCpt($_POST['cpts']);
     $postData->storeAuthorizationInfo();
