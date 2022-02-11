669b138dd08ee5388638f2ca1b7d298bc1d65598
Sherwin <sherwingaddis@gmail.com>
2022-02-11 01:09:41 -0500

    Build prior authorization module saving data

---
+++ interface/modules/custom_modules/oe-module-prior-authorizations/src/Controller/ListAuthorizations.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *  package OpenEMR
+ *  link    https://www.open-emr.org
+ *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  Copyright (c) 2022.
+ *  All Rights Reserved
+ */
+
+namespace Juggernaut\OpenEMR\Modules\PriorAuthModule\Controller;
+
+use OpenEMR\Common\Database\QueryUtils;
+
+class ListAuthorizations
+{
+    private $pid;
+
+    /**
+     * @param mixed $pid
+     */
+    public function setPid($pid): void
+    {
+        $this->pid = $pid;
+    }
+
+    public function __construct()
+    {
+        //do epic stuff
+    }
+
+    public function getAllAuthorizations()
+    {
+        $statement = "SELECT * FROM module_prior_authorizations WHERE `pid` = ?";
+        $binds = array();
+        $binds[] = $this->pid;
+        return QueryUtils::sqlStatementThrowException($statement, $binds);
+    }
+
+
+}
