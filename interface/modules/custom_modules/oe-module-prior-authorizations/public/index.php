ec79173057564e652545fbe78a83047f1754750b
Sherwin <sherwingaddis@gmail.com>
2022-02-23 07:24:37 -0500

    Build prior authorization module adding ability to edit the data

--- interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
@@ -137,7 +137,8 @@ const TABLE_TD = "</td><td>";
                                 print TABLE_TD . $iter['end_date'];
                             }
                             print TABLE_TD . $iter['cpt'];
-                            print TABLE_TD . " <button class='btn btn-primary' onclick=getRowData('" . $editData . "')>" . xlt('Edit'). "</button> </td></tr>";
+                            print TABLE_TD . " <button class='btn btn-primary' onclick=getRowData(" . $iter['id'] . ")>" . xlt('Edit'). "</button>
+                            <input type='hidden' name='" . $iter['id'] . "' value='" . $editData . "' ></td></tr>";
                         }
                     }
                 ?>
