a2eada1bccd812e6ebf1b86194d7dcb31f8f6e7a
Sherwin <sherwingaddis@gmail.com>
2022-03-01 05:04:57 -0500

    Menu changes and adding event

--- interface/modules/custom_modules/oe-module-prior-authorizations/openemr.bootstrap.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/openemr.bootstrap.php
@@ -40,10 +40,10 @@ function oe_module_priorauth_add_menu_item(MenuEvent $event)
 
     return $event;
 }
-/*
+
 function oe_module_priorauth_patient_menu_item(MenuEvent $event)
 {
-    $menu = event->getMenu();
+    $menu = $event->getMenu();
     $menuItem = new stdClass();
     $menuItem->requirement = 0;
     $menuItem->target = 'priorauth';
@@ -61,7 +61,7 @@ function oe_module_priorauth_patient_menu_item(MenuEvent $event)
     $event->setMenu($menu);
     return $event;
 }
-*/
+
 /**
  * @var EventDispatcherInterface $eventDispatcher
  * @var array                    $module
@@ -71,4 +71,4 @@ function oe_module_priorauth_patient_menu_item(MenuEvent $event)
 
 
 $eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_priorauth_add_menu_item');
-//$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_priorauth_patient_menu_item');
+$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_priorauth_patient_menu_item');
