929f9dfb298ac77db941d11ba2d2b5279becf549
Sherwin <sherwingaddis@gmail.com>
2022-03-01 05:54:35 -0500

    Adding report foundation

---
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/reports/list_report.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ *  package OpenEMR
+ *  link    https://www.open-emr.org
+ *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  Copyright (c) 2022.
+ *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+use OpenEMR\Core\Header;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php Header::setupHeader(['common']) ?>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List Exising Prior Auths Report</title>
+</head>
+<body>
+    <div class="container-lg">
+        <h1>Prior Auths</h1>
+    </div>
+</body>
+</html>
