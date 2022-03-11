cfb4e952c25e58cd64d16b745182cb7380a35616
Sherwin <sherwingaddis@gmail.com>
2022-03-11 14:10:34 -0500

    Module import function build continued

--- interface/modules/zend_modules/module/Installer/src/Installer/Controller/ModuleImport.php
+++ interface/modules/zend_modules/module/Installer/src/Installer/Controller/ModuleImport.php
@@ -55,7 +55,7 @@ class ModuleImport
 
     public static function createImportDir()
     {
-        return $import_dir = dirname(__DIR__, 4) ;
+        return $import_dir = dirname(__DIR__, -2) ;
         if (!file_exists($import_dir)) {
             $import_dir = dirname(__DIR__, 6) .  '/custom_modules/';
             try {
