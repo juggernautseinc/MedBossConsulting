78d0668d4ff069140eb6ab4d84cfcea897884064
Sherwin <sherwingaddis@gmail.com>
2022-03-13 15:36:00 -0400

    Module import function build continued GUI build

--- interface/modules/zend_modules/module/Installer/src/Installer/Controller/ModuleImport.php
+++ interface/modules/zend_modules/module/Installer/src/Installer/Controller/ModuleImport.php
@@ -89,7 +89,7 @@ class ModuleImport
     public static function createDestinationFolder($destination)
     {
         if (isset($destination)) {
-            var_dump(!is_dir($destination));
+            var_dump(is_dir($destination));
             return "Is not a directory yet! ";
         } else {
             return "Something else happened";
