3ade1ddfae7034eff54dec4e13a35d5ae04f2ceb
Sherwin <sherwingaddis@gmail.com>
2022-04-08 06:25:10 -0400

    Upgrading the codebase reverting the version file for now

--- version.php
+++ version.php
@@ -15,20 +15,20 @@
 // numbers are stored in the database and used to determine which sql
 // upgrade file is the starting point for the next upgrade.
 $v_major = '6';
-$v_minor = '1';
+$v_minor = '0';
 $v_patch = '0';
 $v_tag   = ''; // minor revision number, should be empty for production releases
 
 // A real patch identifier. This is incremented when we release a patch for a
 // production release. Note the above $v_patch variable is a misnomer and actually
 // stores release version information.
-$v_realpatch = '0';
+$v_realpatch = '3';
 
 // Database version identifier, this is to be incremented whenever there
 // is a database change in the course of development.  It is used
 // internally to determine when a database upgrade is needed.
 //
-$v_database = 436;
+$v_database = 375;
 
 // Access control version identifier, this is to be incremented whenever there
 // is a access control change in the course of development.  It is used
@@ -36,7 +36,7 @@ $v_database = 436;
 // controls is (subsequently the acl_upgrade.php script then is used to
 // upgrade and track this value)
 //
-$v_acl = 12;
+$v_acl = 9;
 
 // Version for JavaScript and stylesheet includes. Increment whenever a .js or .css file changes.
 // Also whenever you change a .js or .css file, make sure that all URLs referencing it
@@ -47,7 +47,7 @@ if (!empty($_ENV['OPENEMR__ENVIRONMENT']) && ($_ENV['OPENEMR__ENVIRONMENT'] === 
     $v_js_includes = md5(microtime());
 } else {
     // Change this number when bumping
-    $v_js_includes = 72;
+    $v_js_includes = 63;
 }
 
 // Do not modify below
