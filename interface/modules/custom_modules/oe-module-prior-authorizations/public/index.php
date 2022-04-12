f33dfde028c77a3926db4511a5cf5714b4f40e7e
Sherwin <sherwingaddis@gmail.com>
2022-04-12 18:04:04 -0400

    Prior Authorization building delete process

--- interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/index.php
@@ -176,7 +176,7 @@ const TABLE_TD = "</td><td>";
     function removeEntry(id) {
         alert('Still Building the process, stop rushing me.');
         let url = 'deleter.php?id=' + encodeURIComponent(id);
-        dlgopen(url, '', 'modal-sm', 275, '',  'Delete auth'{
+        dlgopen(url, 'Delete Auth', 'modal-sm', 275, '',{
             buttons: [
                 {text: 'Done', style: 'danger btn-sm', close: true},
                 {text: 'Cancel', close: true, style: 'default btn-sm'}
