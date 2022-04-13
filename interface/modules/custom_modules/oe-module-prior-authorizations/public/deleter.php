a2d617d1fcf10c63386359867e82fd5a85cc6d40
Sherwin <sherwingaddis@gmail.com>
2022-04-13 14:40:20 -0400

    Prior Authorization building delete process

--- interface/modules/custom_modules/oe-module-prior-authorizations/public/deleter.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/deleter.php
@@ -8,11 +8,7 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-
+require_once dirname(__FILE__, 5) . '/globals.php';
 sqlQuery("delete from `module_prior_authorizations` where `id` = ?", [$_GET['id']]);
 
 ?>
