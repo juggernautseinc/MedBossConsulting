4988718592e43e93b14e2ec949a44e5597307e92
Sherwin <sherwingaddis@gmail.com>
2022-04-13 14:29:37 -0400

    Prior Authorization building delete process

--- interface/modules/custom_modules/oe-module-prior-authorizations/public/deleter.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/public/deleter.php
@@ -9,6 +9,23 @@
  */
 
 
-echo $_GET['id'];
 
-echo "<br> <br> close this window! your letting the air out!";
+
+sqlQuery("delete from `module_prior_authorizations` where `id` = ?", [$_GET['id']]);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo xlt('Delete Record') ?></title>
+</head>
+<body>
+    <p><?php echo "<br> <br>" .  xlt("If you are seeing this message the record was deleted"); ?></p>
+</body>
+</html>
+
+
