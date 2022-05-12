067621528555f743f22ce48f074d8f448d6ef45c
Sherwin <sherwingaddis@gmail.com>
2022-05-12 10:30:53 -0400

    Adding fuction to resend login-errors

--- library/bill_login_error_reprocess.php
+++ library/bill_login_error_reprocess.php
@@ -13,7 +13,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+require_once dirname(__FILE__, 2) . '/interface/globals.php';
 
 use OpenEMR\Billing\BillingProcessor\X12RemoteTracker;
 
