16676c4f414162b0c42d5971399407f0bad67892
Sherwin <sherwingaddis@gmail.com>
2022-05-22 22:52:04 -0400

    Working on the router again!

--- interface/modules/custom_modules/text-messaging-app/welcome.php
+++ interface/modules/custom_modules/text-messaging-app/welcome.php
@@ -26,7 +26,7 @@ use OpenEMR\Core\Header;
 <body>
     <div class="container m-5">
         <h1><?php echo xlt('Texting Patients'); ?></h1>
-        <!-- <a href="public/index.php/invoices?foo=bar" ><?php echo xlt('Send a message'); ?></a>-->
+        <a href="public/index.php/invoices?foo=bar" ><?php echo xlt('Index Page Go!'); ?></a>
         <input type="text" name="pnumbers" value="" class="form-control" >
         <textarea name="message" class="form-control"></textarea>
         <input class="btn btn-primary" type="submit" value="Send Message">
