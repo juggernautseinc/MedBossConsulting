59ace8a5b9f11f1fcfe2628d2c5cdb47779007db
Sherwin <sherwingaddis@gmail.com>
2022-05-24 17:19:50 -0400

    TextApp building bulk message send

--- interface/modules/custom_modules/text-messaging-app/src/App/Controllers/Texting.php
+++ interface/modules/custom_modules/text-messaging-app/src/App/Controllers/Texting.php
@@ -1,10 +1,11 @@
 <?php
+
 /*
  *  package OpenEMR
  *  link    https://www.open-emr.org
  *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
  *  Copyright (c) 2022.
- *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *  All rights reserved
  */
 
 namespace Juggernaut\App\Controllers;
@@ -13,8 +14,12 @@ class Texting
 {
     public static function bulk(): void
     {
-        echo $numbers = $_POST['pnumbers'];
-        echo $messagesbody = $_POST['message'];
+        $numbers = $_POST['pnumbers'];
+        $messagesbody = $_POST['message'];
+
+        foreach ($numbers as $number) {
+            echo $number;
+        }
 
     }
 }
