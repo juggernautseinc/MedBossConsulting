f7180f8c60699f5c2eeb25ec27229f6a8f5393af
Sherwin <sherwingaddis@gmail.com>
2022-05-24 17:59:03 -0400

    TextApp building bulk message send

--- interface/modules/custom_modules/text-messaging-app/welcome.php
+++ interface/modules/custom_modules/text-messaging-app/welcome.php
@@ -30,8 +30,8 @@ use OpenEMR\Core\Header;
         <a href="public/index.php/home" ><?php //echo xlt('Home Page Go!'); ?></a>
         <a href="public/index.php/invoices/create" ><?php //echo xlt('Create Invoices Page Go!'); ?></a>-->
         <form method="post" action="public/index.php/texting/bulk" name="textcrude" >
-            <input type="text" name="pnumbers" value="" class="form-control" placeholder="place commas between numbers">
-            <textarea name="message" class="form-control" placeholder="Enter mass message here"></textarea>
+            <input type="text" name="pnumbers" value="" class="form-control m-2" placeholder="place commas between numbers">
+            <textarea name="message" class="form-control m-2" placeholder="Enter mass message here"></textarea>
             <input class="btn btn-primary mt-3" type="submit" value="Send Message">
         </form>
     </div>
