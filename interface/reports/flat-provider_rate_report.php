7548751374f2719e3d7ee63fd7eda64e075f3ad5
Sherwin <sherwingaddis@gmail.com>
2022-05-26 08:00:53 -0400

    Payroll stuff rounding off - fixed

--- interface/reports/flat-provider_rate_report.php
+++ interface/reports/flat-provider_rate_report.php
@@ -113,7 +113,7 @@ function endDoctor(&$docrow)
     echo "   &nbsp;";
     $pay = bucks($rate['flat'] * text($docrow['encounters']));
     $mpay = $rate['flat'] * $docrow['encounters'];
-    echo   $rate['flat'] . " $" . $pay;
+    echo   $rate['flat'] . " $" . $mpay;
     echo text($docrow['encounters']);
     echo "&nbsp;\n";
     echo "  </td>\n";
