2bdedfc865798ab512aac534392e7ab68b1caa8a
Sherwin <sherwingaddis@gmail.com>
2022-05-27 13:30:50 -0400

    Revisiting router configuration build not right yet

--- interface/modules/custom_modules/text-messaging-app/public/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/index.php
@@ -6,7 +6,7 @@ use Juggernaut\App\Controllers\Texting;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 
 /**
  *  package OpenEMR
