3e1b5426af04c3b8b3a225c2825a0fb25a5f95a2
Sherwin <sherwingaddis@gmail.com>
2022-05-27 10:59:24 -0400

    Revisiting router configuration build not right yet

--- interface/modules/custom_modules/text-messaging-app/src/App/Controllers/Home.php
+++ interface/modules/custom_modules/text-messaging-app/src/App/Controllers/Home.php
@@ -12,7 +12,7 @@ namespace Juggernaut\App\Controllers;
 
 class Home
 {
-    public static function index(): string
+    public function index(): string
     {
         return '<!doctype html><html><title>Home page</title><body><h1>Home</h1></body></html>';
     }
