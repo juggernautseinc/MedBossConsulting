6faae23b71ce7e87f50da84b0f2aa576f4a3ec0d
Sherwin <sherwingaddis@gmail.com>
2022-05-29 19:17:44 -0400

    Response API build in process

--- interface/modules/custom_modules/text-messaging-app/public/api/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/api/index.php
@@ -16,7 +16,7 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Access-Control-Allow
 
 
 require_once dirname(__DIR__, 5) . "/globals.php";
-require_once __DIR__ . '/../../vendor/autoload.php';
+//require_once __DIR__ . '/../../vendor/autoload.php';
 http_response_code(200);
 die('here');
 use OpenEMR\Common\Crypto\CryptoGen;
