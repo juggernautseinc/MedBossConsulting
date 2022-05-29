0e5d904f71c2bbef13853d8dcb2cf45045c38439
Sherwin <sherwingaddis@gmail.com>
2022-05-29 18:57:24 -0400

    Response API build in process

--- interface/modules/custom_modules/text-messaging-app/public/api/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/api/index.php
@@ -11,7 +11,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Access-Control-Allow-Headers, Authorizations, X-Requested-With');
 
 use OpenEMR\Common\Crypto\CryptoGen;
 $key = new CryptoGen();
@@ -22,5 +22,9 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
+file_put_contents("/var/www/html/errors/uriFile.txt", print_r($uri, true));
+http_response_code(200);
 
-echo json_encode($uri);
+EventAuditLogger::instance()->newEvent('text', '', '', 1, "Inbound Text received");
+
+
