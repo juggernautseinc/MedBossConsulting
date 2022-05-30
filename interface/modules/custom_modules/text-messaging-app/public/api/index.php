dd8ea77929e6fa21f725cb763b9423aff0f60404
Sherwin <sherwingaddis@gmail.com>
2022-05-29 20:04:36 -0400

    Response API build in process

--- interface/modules/custom_modules/text-messaging-app/public/api/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/api/index.php
@@ -22,6 +22,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use OpenEMR\Common\Crypto\CryptoGen;
 use OpenEMR\Common\Logging\EventAuditLogger;
+use Juggernaut\App\Controllers\apiResponse;
 
 $key = new CryptoGen();
 if (!defined('CONST_INCLUDE_KEY')) {define('CONST_INCLUDE_KEY', $key->decryptStandard($GLOBALS['response_key']));}
@@ -30,7 +31,15 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
 file_put_contents("/var/www/html/errors/uriFile.txt", print_r($uri, true));
-http_response_code(200);
+
+if ($uri[7] === 'reply') {
+    $res = apiResponse::getResponse('200');
+    echo json_encode($res);
+} else {
+    $res = apiResponse::getResponse('400');
+    echo json_encode($res);
+}
+
 
 EventAuditLogger::instance()->newEvent('text', '', '', 1, "Inbound Text received");
 
