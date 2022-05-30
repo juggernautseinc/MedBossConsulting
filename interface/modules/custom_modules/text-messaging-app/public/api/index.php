4b285399111781f24628572f7fe4d23abc301cb0
Sherwin <sherwingaddis@gmail.com>
2022-05-29 20:11:05 -0400

    Response API build in process

--- interface/modules/custom_modules/text-messaging-app/public/api/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/api/index.php
@@ -30,7 +30,7 @@ if (!defined('CONST_INCLUDE_KEY')) {define('CONST_INCLUDE_KEY', $key->decryptSta
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
-file_put_contents("/var/www/html/errors/uriFile.txt", print_r($uri, true));
+file_put_contents("/var/www/html/errors/uriFile.txt", print_r($_POST, true));
 
 if ($uri[7] === 'reply') {
     $res = apiResponse::getResponse('200');
