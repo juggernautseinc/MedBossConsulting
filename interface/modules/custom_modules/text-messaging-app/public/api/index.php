8092c5f42ebc9d60cefbb6ca496ed2fd8f87658d
Sherwin <sherwingaddis@gmail.com>
2022-05-31 12:17:02 -0400

    Response API build in process - trying to resolve inbound replies

--- interface/modules/custom_modules/text-messaging-app/public/api/index.php
+++ interface/modules/custom_modules/text-messaging-app/public/api/index.php
@@ -30,8 +30,9 @@ if (!defined('CONST_INCLUDE_KEY')) {define('CONST_INCLUDE_KEY', $key->decryptSta
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
-
-file_put_contents("/var/www/html/errors/uriFile.txt", 'php//input');
+$json = file_get_contents('php//input');
+$data = json_decode($json);
+file_put_contents("/var/www/html/errors/uriFile.txt", $data);
 
 if ($uri[7] === 'reply') {
     $res = apiResponse::getResponse('200');
