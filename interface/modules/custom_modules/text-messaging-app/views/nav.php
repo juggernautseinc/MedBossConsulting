8ec6bf157d2b9ea1a89bd78fdf38a6c5fd5f5976
Sherwin <sherwingaddis@gmail.com>
2022-06-03 04:44:38 -0400

    View text build in process - Invoice render - adding navigation

--- interface/modules/custom_modules/text-messaging-app/views/nav.php
+++ interface/modules/custom_modules/text-messaging-app/views/nav.php
@@ -10,6 +10,6 @@
 ?>
 
 <div>
-    <a href="<?php echo $GLOBALS['webroot'] . __DIR__ ?>/../welcome.php" class="btn btn\"><?php echo xlt('Home'); ?></a>
+    <a href="<?php echo $GLOBALS['webroot'] . dirname(__DIR__, 2) ?>/welcome.php" class="btn btn\"><?php echo xlt('Home'); ?></a>
 </div>
 
