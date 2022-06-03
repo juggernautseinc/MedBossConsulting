e1d058bc408b356b2d12e528c570fbdb1c533727
Sherwin <sherwingaddis@gmail.com>
2022-06-03 03:35:08 -0400

    View text build in process - TS

--- interface/modules/custom_modules/text-messaging-app/src/App/View.php
+++ interface/modules/custom_modules/text-messaging-app/src/App/View.php
@@ -26,7 +26,7 @@ class View
      */
     public function render(): string
     {
-        return 'here from view';
+        return 'here from view ' . $this->view;
         /*$viewFile = VIEW_PATH . '/' . $this->view . '.php';
         if (! file_exists($viewFile)) {
             throw new ViewNotFoundException();
