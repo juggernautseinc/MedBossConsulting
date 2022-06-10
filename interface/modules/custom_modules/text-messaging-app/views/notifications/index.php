80a4ee19af862843a4162d44d606a14da12e9ffa
Sherwin <sherwingaddis@gmail.com>
2022-06-10 10:00:25 -0400

    exposing notification screen

--- interface/modules/custom_modules/text-messaging-app/views/notifications/index.php
+++ interface/modules/custom_modules/text-messaging-app/views/notifications/index.php
@@ -28,11 +28,11 @@ $sec = "10";
 <body>
     <div class="container-fluid main-container m-5 w-auto">
         <div class="row">
-            <div class="" style="float: left">
+            <div class="">
                 <h1><?php echo xlt('Notifications'); ?></h1>
-            </div>
-            <div id="contactpatient" style="float: right">
-                <button id="sendMessage"><?php echo xlt('Text Patient'); ?></button>
+                <div id="contactpatient" style="float: right">
+                    <button class="btn btn-primary" id="sendMessage"><?php echo xlt('Text Patient'); ?></button>
+                </div>
             </div>
             <table class="table table-striped">
                 <tr>
