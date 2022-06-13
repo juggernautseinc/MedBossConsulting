8d7ffc036041016ac1397a2050cfc4b0ab00de16
Sherwin <sherwingaddis@gmail.com>
2022-06-12 23:41:47 -0400

    Preparing for module launch

--- interface/modules/custom_modules/text-messaging-app/settings.php
+++ interface/modules/custom_modules/text-messaging-app/settings.php
@@ -30,7 +30,7 @@ require_once dirname(__DIR__, 3) . '/globals.php';
             <div class="mx-auto">
                 <strong><?php echo $GLOBALS['SMS_NOTIFICATION_HOUR'] . " " . xlt(' Hours in advanced to send notification - in Globals') ?></strong>
                 <?php require_once dirname(__FILE__) . "/views/nav_top.php"; ?>
-                <?php echo SendMessage::IsValidFQDN($_SERVER['HTTP_HOST']); ?>
+                <?php echo $_SERVER['HTTP_HOST'] . " " . SendMessage::IsValidFQDN($_SERVER['HTTP_HOST']); ?>
             </div>
         </div>
 
