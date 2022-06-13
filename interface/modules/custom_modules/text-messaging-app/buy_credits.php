39e271053f982184c079a0b8224888f7aa5784c0
Sherwin <sherwingaddis@gmail.com>
2022-06-12 22:53:55 -0400

    Preparing for module launch

--- interface/modules/custom_modules/text-messaging-app/buy_credits.php
+++ interface/modules/custom_modules/text-messaging-app/buy_credits.php
@@ -22,13 +22,14 @@ require_once dirname(__DIR__, 3) . '/globals.php';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo xlt('Purchase Credits'); ?></title>
+    <?php Header::setupHeader(['common']); ?>
 </head>
 <body>
     <div class="container-fluid main_container mt-3">
         <div class="row">
             <div class="mx-auto">
                 <h3><?php echo xlt('Buy Texting Credits'); ?></h3>
-                <?php require_once dirname(__FILE__) . "views/nav_top.php"; ?>
+                <?php require_once dirname(__FILE__) . "/views/nav_top.php"; ?>
                 <div >
                 $6   50     https://link.waveapps.com/jtju7p-3c36rt
 
