ea7dab052c91e77aaf31887f22fa5f75306e1559
Sherwin <sherwingaddis@gmail.com>
2022-06-14 17:17:28 -0400

    Setting services for LBF PROVIDER ID

--- library/FeeSheetHtml.class.php
+++ library/FeeSheetHtml.class.php
@@ -56,8 +56,8 @@ class FeeSheetHtml extends FeeSheet
         while ($row = sqlFetchArray($res)) {
             $provid = $row['id'];
             $s .= "<option value='" . attr($provid) . "'";
-            if ($provid == $default) {
-                $s .= " selected";
+            if ($provid == $_SESSION['authUserID']) {
+                $s .= " selected";   //changed by sherwin 06/14/2022
             }
 
             $s .= ">";
