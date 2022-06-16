670e7071faea50073a8281c9f26cff5a49879905
Sherwin <sherwingaddis@gmail.com>
2022-06-16 14:32:08 -0400

    Adding population of the date when form opens

---
+++ sites/serenity/LBF/LBT_authorizations.plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ *  package OpenEMR
+ *  link    https://www.open-emr.org
+ *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  Copyright (c) 2022.
+ *  Reserved
+ */
+
+// The purpose of this function is to create JavaScript that is run
+// once when the page is loaded.
+//
+function LBTref_javascript_onload()
+{
+
+    $today = date('Y-m-d H:s:i');
+    echo "
+     document.getElementByID('form_Date_V').value = $today
+    ";
+}
