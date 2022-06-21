1dc7b0213ab087fcdf84a1406db77effd8ed3f66
Sherwin <sherwingaddis@gmail.com>
2022-06-21 09:47:34 -0400

    setting patient inactive database entry

--- interface/patient_file/summary/patient_status.php
+++ interface/patient_file/summary/patient_status.php
@@ -17,6 +17,6 @@ if (!empty($_POST)) {
         CsrfUtils::csrfNotVerified();
     }
 $mark = "INSERT INTO `patient_status` (`statusid`, `status`, `pid`, `userId`, `date`) VALUES ('', 'inactive', ?, ?, NOW())";
-    sqlStatement($mark, [$_POST['patientid'], $_SESSION['authUserId']]);
+    sqlStatement($mark, [$_POST['patientid'], $_SESSION['authUser']]);
 echo 'Patient ID ' . $_POST['patientid']  . ' has been marked inactive';
 }
