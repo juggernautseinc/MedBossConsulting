86d2dcde57fb68e6adc233f890ce95b8268eb467
Sherwin <sherwingaddis@gmail.com>
2022-06-21 08:35:04 -0400

    setting patient inactive

--- interface/patient_file/summary/patient_status.php
+++ interface/patient_file/summary/patient_status.php
@@ -8,7 +8,7 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-require_once "globals.php";
+require_once dirname(__DIR__, 2) . "/globals.php";
 
 if (!empty($_POST)) {
 echo 'safe!';
