4b0f107efb22a8a19bee37b58bfc8988d7ef1b4d
Sherwin <sherwingaddis@gmail.com>
2022-06-22 05:34:50 -0400

    setting patient inactive - creating event to update menu

--- src/Menu/PatientMenuRole.php
+++ src/Menu/PatientMenuRole.php
@@ -232,7 +232,7 @@ EOT;
                 </ul>
             </div>
 EOB;
-        $str_bot .= $dispatcher->dispatch(new PatientMenuLoadEvent(), PatientMenuLoadEvent::EVENT_PATIENT_MENU_LOAD);
+        //$str_bot .= $dispatcher->dispatch(new PatientMenuLoadEvent(), PatientMenuLoadEvent::EVENT_PATIENT_MENU_LOAD);
         $str_bot .= <<<EOB
             <div>
                <button class="btn btn-danger" id="changePatientStatus">Mark Inactive</button>
