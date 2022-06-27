114c3cf90214e3656e883b48bd7f1d68a305a5b9
Sherwin <sherwingaddis@gmail.com>
2022-06-27 03:07:57 -0400

    Building SMS notifier - display array

--- interface/modules/custom_modules/text-messaging-app/src/App/Model/NotificationModel.php
+++ interface/modules/custom_modules/text-messaging-app/src/App/Model/NotificationModel.php
@@ -54,9 +54,10 @@ class NotificationModel
     {
         $nDays = self::numberOfDays();
         $date = date("Y-m-d",strtotime($nDays));
-        $sql = "SELECT `pe`.`pc_pid`, `pe`.`pc_eid`, `pe`.`pc_aid`, `pe`.`pc_catid`, `pd`.`phone_cell`, `pd`.`fname` FROM `openemr_postcalendar_events` pe
+        $sql = "SELECT `pe`.`pc_pid`, `pe`.`pc_eid`, `pe`.`pc_aid`, `pe`.`pc_catid`, `pe`.`pc_startTime`, `pd`.`phone_cell`, `pd`.`fname`
+                                                FROM `openemr_postcalendar_events` pe
                                                 LEFT JOIN `patient_data` pd ON `pd`.`pid` = `pe`.`pc_pid`
-                                                WHERE `pe`.`pc_eventDate` BETWEEN ? AND ?";
+                                                WHERE `pe`.`pc_eventDate` BETWEEN ? AND ? AND `pe`.`pc_apptstatus` != 'x'";
         $genListOfAppointment = sqlStatement($sql, [$date, $date]);
         $listOfAppointments = [];
         while($row = sqlFetchArray($genListOfAppointment)) {
