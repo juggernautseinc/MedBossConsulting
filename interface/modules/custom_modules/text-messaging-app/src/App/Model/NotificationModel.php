94f3eb4e4aa998ea3309cd48030f4ba8210faaba
Sherwin <sherwingaddis@gmail.com>
2022-06-27 03:09:30 -0400

    Building SMS notifier - display array

--- interface/modules/custom_modules/text-messaging-app/src/App/Model/NotificationModel.php
+++ interface/modules/custom_modules/text-messaging-app/src/App/Model/NotificationModel.php
@@ -54,7 +54,7 @@ class NotificationModel
     {
         $nDays = self::numberOfDays();
         $date = date("Y-m-d",strtotime($nDays));
-        $sql = "SELECT `pe`.`pc_pid`, `pe`.`pc_eid`, `pe`.`pc_aid`, `pe`.`pc_catid`, `pe`.`pc_startTime`, `pd`.`phone_cell`, `pd`.`fname`
+        $sql = "SELECT `pe`.`pc_pid`, `pe`.`pc_eid`,  `pe`.`pc_eventDate`, `pe`.`pc_aid`, `pe`.`pc_catid`, `pe`.`pc_startTime`, `pd`.`phone_cell`, `pd`.`fname`
                                                 FROM `openemr_postcalendar_events` pe
                                                 LEFT JOIN `patient_data` pd ON `pd`.`pid` = `pe`.`pc_pid`
                                                 WHERE `pe`.`pc_eventDate` BETWEEN ? AND ? AND `pe`.`pc_apptstatus` != 'x'";
