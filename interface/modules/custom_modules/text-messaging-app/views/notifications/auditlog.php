d04274e395fa5b716f4f48f387f1956227daacd7
juggernautsei <sherwingaddis@gmail.com>
2022-07-03 15:18:18 -0400

    building the audit log for appointment notification.

--- interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
+++ interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
@@ -21,7 +21,7 @@ use OpenEMR\Core\Header;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo xlt('Audit Log'); ?></title>
-    <?php Header::setupHeader(['common']) ?>
+    <?php Header::setupHeader(['common',  'datatables', 'datatables-dt', 'datatables-bs', 'datatables-scroller']) ?>
 </head>
 <body>
 <div class="container-main m-5">
