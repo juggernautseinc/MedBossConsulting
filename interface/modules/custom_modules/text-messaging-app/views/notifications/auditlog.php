3c4b548ad7d19fa7309cd7b68eac65d47168e893
juggernautsei <sherwingaddis@gmail.com>
2022-07-03 16:10:38 -0400

    building the audit log for appointment notification.

--- interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
+++ interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
@@ -41,7 +41,7 @@ use OpenEMR\Core\Header;
             <tr>
                 <th scope="col"><?php echo xlt("iLogId"); ?></th>
                 <th scope="col"><?php echo xlt("Status"); ?></th>
-                <th scope="col" style='width=600px'><?php echo xlt("Patient"); ?></th>
+                <th scope="col" style='width: 600px'><?php echo xlt("Patient"); ?></th>
                 <th scope="col"><?php echo xlt("Date Time Sent"); ?></th>
                 <th scope="col"><?php echo xlt("Appointment Date"); ?></th>
             </tr>
