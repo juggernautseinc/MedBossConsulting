14816f7eec919e488106ede7c00828df7e4dc2b6
juggernautsei <sherwingaddis@gmail.com>
2022-07-02 23:19:44 -0400

    building the audit log for appointment notification.

--- interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
+++ interface/modules/custom_modules/text-messaging-app/views/notifications/auditlog.php
@@ -8,6 +8,23 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-echo "Show Log";
+use OpenEMR\Core\Header;
 
+?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo xlt('Audit Log'); ?></title>
+    <?php Header::setupHeader(['common']) ?>
+</head>
+<body>
+<div class="container-fluid container-main m-5">
+    <h1><?php echo xlt('Audit Log'); ?></h1>
+</div>
+</body>
+</html>
