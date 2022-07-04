ee608f9ed798aba98ad6b936a6291b52568617e1
juggernautsei <sherwingaddis@gmail.com>
2022-07-04 13:58:09 -0400

    foundation for the financial report graphical

--- interface/modules/custom_modules/oe-module-financial-graph/index.php
+++ interface/modules/custom_modules/oe-module-financial-graph/index.php
@@ -12,7 +12,8 @@ require_once dirname(__FILE__, 4) . "/globals.php";
 
 $paymentsforthemonth = "SELECT ar_activity.pay_amount, ar_activity.adj_amount FROM `ar_activity`
 LEFT JOIN ar_session ON ar_session.session_id = ar_activity.session_id
-WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN '2022-05-01' AND '2022-05-31' AND ar_session.payer_id = 106";
+WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN '2022-05-01' AND '2022-05-31' AND ar_session.payer_id = 106
+AND deposit_date IS NOT NULL";
 
 $totalpayments = sqlStatement($paymentsforthemonth);
 
