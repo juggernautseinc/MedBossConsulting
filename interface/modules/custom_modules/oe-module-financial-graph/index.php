0800398fdf149b6d556ebbec3bd56f66cab0fc63
juggernautsei <sherwingaddis@gmail.com>
2022-07-04 15:19:09 -0400

    foundation for the financial report graphical

--- interface/modules/custom_modules/oe-module-financial-graph/index.php
+++ interface/modules/custom_modules/oe-module-financial-graph/index.php
@@ -14,7 +14,7 @@ function insuranceIncome($beginningDepositDate, $endingDepositDate, $insurersId)
 {
     $paymentsforthemonth = "SELECT (ar_activity.pay_amount - ar_activity.adj_amount) AS net FROM `ar_activity`
     LEFT JOIN ar_session ON ar_session.session_id = ar_activity.session_id
-    WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN ? AND ? AND ar_session.payer_id = 106
+    WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN ? AND ? AND ar_session.payer_id = ?
     AND ar_session.deposit_date IS NOT NULL AND ar_activity.deleted IS NULL";
 
     $totalpayments = sqlStatement($paymentsforthemonth, [$beginningDepositDate, $endingDepositDate, $insurersId]);
