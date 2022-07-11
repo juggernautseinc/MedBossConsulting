a045c1665d885123e0772d0e968f9cfc5e3148c1
juggernautsei <sherwingaddis@gmail.com>
2022-07-10 22:31:28 -0400

    working on the financial graph for year to date income from insurance companies - configuring module setting

--- interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs/chart-types/line.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs/chart-types/line.php
@@ -1,11 +1,12 @@
 <?php
 
-//require_once dirname(__FILE__, 6) . "/../globals.php";
+
 require_once dirname(__FILE__, 3) . "/../vendor/autoload.php";
 
 include '../headers.php';
 include '../sidebars.php';
 include '../content.php';
+require_once dirname(__FILE__, 6) . "/../globals.php";
 
 use Juggernaut\App\MonthlyIncomeDataPoints;
 
