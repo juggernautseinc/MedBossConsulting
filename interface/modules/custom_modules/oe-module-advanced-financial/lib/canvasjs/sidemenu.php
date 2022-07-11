f4cc7574ada6827f549b1171230525a3a9d1d299
juggernautsei <sherwingaddis@gmail.com>
2022-07-10 23:12:37 -0400

    working on the financial graph for year to date income from insurance companies - configuring module setting and menu

---
+++ interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs/sidemenu.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *  package OpenEMR
+ *  link    https://www.open-emr.org
+ *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  Copyright (c) 2022.
+ *  All rights reserved
+ */
+
+?>
+<!-- href links different in integration, collaspe in class added server side in integration -->
+<!-- sidebar -->
+<div id="sidebar" class="sidebar-toggle">
+    <ul class="nav nav-sidebar">
+        <li>
+            <a href="../canvasjs">
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <span>HOME</span>
+            </a>
+        </li>
+    </ul>
+</div>
