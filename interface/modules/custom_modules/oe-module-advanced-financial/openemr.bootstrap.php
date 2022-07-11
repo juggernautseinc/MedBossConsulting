7bc787c87cfd8d57628ec7597586d16b1b93a7da
juggernautsei <sherwingaddis@gmail.com>
2022-07-10 22:47:12 -0400

    working on the financial graph for year to date income from insurance companies - configuring module setting and menu

--- interface/modules/custom_modules/oe-module-advanced-financial/openemr.bootstrap.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/openemr.bootstrap.php
@@ -41,6 +41,31 @@ function oe_module_financialview_add_menu_item(MenuEvent $event)
     return $event;
 }
 
+function oe_module_adv_financials_add_menu_item(MenuEvent $event)
+{
+    $menu = $event->getMenu();
+
+    $menuItem = new stdClass();
+    $menuItem->requirement = 0;
+    $menuItem->target = 'rep';
+    $menuItem->menu_id = 'avf1';
+    $menuItem->label = xlt("Advance Financial");
+    $menuItem->url = "/interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs";
+    $menuItem->children = [];
+    $menuItem->acl_req = ["patients", "docs"];
+    $menuItem->global_req = [];
+
+    foreach ($menu as $item) {
+        if ($item->menu_id == 'finimg') {
+            $item->children[] = $menuItem;
+            break;
+        }
+    }
+
+    $event->setMenu($menu);
+
+    return $event;
+}
 /**
  * @var EventDispatcherInterface $eventDispatcher
  * @var array                    $module
@@ -49,3 +74,4 @@ function oe_module_financialview_add_menu_item(MenuEvent $event)
  */
 
 $eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_financialview_add_menu_item');
+$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_adv_financials_add_menu_item');
