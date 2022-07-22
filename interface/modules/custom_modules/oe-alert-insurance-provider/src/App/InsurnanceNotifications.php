fec4f8b0402ce550a246624d61939a0ab346abf0
juggernautsei <sherwingaddis@gmail.com>
2022-07-22 09:50:56 -0400

    storing the letter as PDF in temp folder - testing conversion to PDF

--- interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
+++ interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
@@ -31,7 +31,7 @@ class InsuranceNotifications
             $this->letter = $document->letterTemplate();
         }
 
-        file_put_contents("/var/www/html/errors/returnedToPdfit-" . date('Y-m-d H:m:s') . ".txt", $this->letter);
+        file_put_contents("/var/www/html/errors/returnedToPdfit-" . date('Y-m-d H:m:s') . ".html", $this->letter);
         //$this->pdf = self::convertHtmlToPdf();
         //self::storeTempPdfDocument();
     }
