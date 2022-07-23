375db17a900df0a5bbb5a628e31efd171ee3583a
juggernautsei <sherwingaddis@gmail.com>
2022-07-23 06:02:27 -0400

    fixing encounter report

--- interface/reports/encounters_report.php
+++ interface/reports/encounters_report.php
@@ -512,10 +512,10 @@ if (!empty($_POST['form_refresh']) || !empty($_POST['form_orderby'])) {
            <?php
            while ($rowresult4 = sqlFetchArray($result4)) {
            ?>
-               EncounterIdArray[<?php echo attr($iter['enc_pid']); ?>][Count] = <?php echo js_escape($rowresult4['encounter']); ?>;
-               EncounterDateArray[<?php echo attr($iter['enc_pid']); ?>][Count] = <?php echo js_escape(oeFormatShortDate(date("Y-m-d", strtotime($rowresult4['date'])))); ?>;
-               CalendarCategoryArray[<?php echo attr($iter['enc_pid']); ?>][Count] = <?php echo js_escape(xl_appt_category($rowresult4['pc_catname'])); ?>;
-               EncounterNoteArray[<?php echo attr($iter['enc_pid']); ?>][Count] = <?php echo js_escape($rowresult4['billing_note']); ?>;
+               EncounterIdArray[<?php echo attr($row['pid']); ?>][Count] = <?php echo js_escape($rowresult4['encounter']); ?>;
+               EncounterDateArray[<?php echo attr($row['pid']); ?>][Count] = <?php echo js_escape(oeFormatShortDate(date("Y-m-d", strtotime($rowresult4['date'])))); ?>;
+               CalendarCategoryArray[<?php echo attr($row['pid']); ?>][Count] = <?php echo js_escape(xl_appt_category($rowresult4['pc_catname'])); ?>;
+               EncounterNoteArray[<?php echo attr($row['pid']); ?>][Count] = <?php echo js_escape($rowresult4['billing_note']); ?>;
                Count++;
            <?php
            $enc_billing_note[$rowresult4['encounter']] = $rowresult4['billing_note'];
