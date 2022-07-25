47060ebc8e095764bce45b1d2a3dec75c34e697b
juggernautsei <sherwingaddis@gmail.com>
2022-07-25 17:52:30 -0400

    trying to create a PDF from the template

--- interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
+++ interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
@@ -36,7 +36,25 @@ class InsuranceNotifications
             file_put_contents("/var/www/html/errors/" . $this->pid . "-" . date('Y-m-d_H:m:s') . ".html", $this->letter);
         }
         $this->pdfName = date('Y-m-d_H:m:s') . ".pdf";
-        $mpdf = new Mpdf();
+        $config_mpdf = array(
+            'tempDir' => $GLOBALS['MPDF_WRITE_DIR'],
+            'mode' => $GLOBALS['pdf_language'],
+            'format' => $GLOBALS['pdf_size'],
+            'default_font_size' => '9',
+            'default_font' => 'dejavusans',
+            'margin_left' => $GLOBALS['pdf_left_margin'],
+            'margin_right' => $GLOBALS['pdf_right_margin'],
+            'margin_top' => $GLOBALS['pdf_top_margin'],
+            'margin_bottom' => $GLOBALS['pdf_bottom_margin'],
+            'margin_header' => '',
+            'margin_footer' => '',
+            'orientation' => $GLOBALS['pdf_layout'],
+            'shrink_tables_to_fit' => 1,
+            'use_kwt' => true,
+            'autoScriptToLang' => true,
+            'keep_table_proportions' => true
+        );
+        $mpdf = new Mpdf($config_mpdf);
         $mpdf->WriteHTML($this->letter);
         $this->output = $mpdf->Output($this->pdfName);
         file_put_contents("/var/www/html/errors/" . $this->pid . "-" . date('Y-m-d_H:m:s') . ".pdf", $this->output);
