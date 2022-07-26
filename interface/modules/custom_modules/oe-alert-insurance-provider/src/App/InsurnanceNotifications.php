40ab6edb0bda64230f7fd7652680a0c0f7f0fc93
juggernautsei <sherwingaddis@gmail.com>
2022-07-26 06:00:02 -0400

    attempt to store file in patient chart

--- interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
+++ interface/modules/custom_modules/oe-alert-insurance-provider/src/App/InsurnanceNotifications.php
@@ -10,6 +10,7 @@
 
 namespace Juggernaut\App;
 
+use GuzzleHttp\Client;
 use OpenEMR\Pdf\PdfCreator;
 use Mpdf\Mpdf;
 
@@ -35,78 +36,21 @@ class InsuranceNotifications
         if ($this->checkInsurance && !empty($contact[1]['field_value'])) {
              //fill out template if the contact form has an email address
             $this->letter = $document->letterTemplate();
-            file_put_contents("/var/www/html/errors/" . $this->pid . "-" . date('Y-m-d_H:m:s') . ".pdf", $this->letter);
+            file_put_contents("/var/www/html/errors/" . $this->pid . "-" . date('Y-m-d_H:m:s') . ".html", $this->letter);
         }
-        $this->pdfName = date('Y-m-d_H:m:s') . ".pdf";
-        $config_mpdf = array(
-            'tempDir' => $GLOBALS['MPDF_WRITE_DIR'],
-            'mode' => $GLOBALS['pdf_language'],
-            'format' => $GLOBALS['pdf_size'],
-            'default_font_size' => '9',
-            'default_font' => 'dejavusans',
-            'margin_left' => $GLOBALS['pdf_left_margin'],
-            'margin_right' => $GLOBALS['pdf_right_margin'],
-            'margin_top' => $GLOBALS['pdf_top_margin'],
-            'margin_bottom' => $GLOBALS['pdf_bottom_margin'],
-            'margin_header' => '',
-            'margin_footer' => '',
-            'orientation' => $GLOBALS['pdf_layout'],
-            'shrink_tables_to_fit' => 1,
-            'use_kwt' => true,
-            'autoScriptToLang' => true,
-            'keep_table_proportions' => true
-        );
-        $mpdf = new Mpdf($config_mpdf);
-        //$mpdf->WriteHTML(__toString($this->letter));
-        //$this->output = $mpdf->Output($this->pdfName);
-        //file_put_contents("/var/www/html/errors/" . $this->pid . "-" . date('Y-m-d_H:m:s') . ".pdf", $this->output);
 
-        //self::storeTempPdfDocument();
+        self::storeTempPdfDocument();
     }
 
     protected function storeTempPdfDocument(): void
     {
-        $postlocation = dirname(__DIR__, 6) . "/controller.php?document&upload&patient_id=" . $this->pid . "&parent_id=685461&";
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('POST', $postlocation, [
+        $postLocation = dirname(__DIR__, 6) . "/controller.php?document&upload&patient_id=" . $this->pid . "&parent_id=685461&";
+        $client = new Client();
+        $response = $client->request('POST', $postLocation, [
             'multipart' => [
                 'name' => $this->pdfName,
                 'contents' => $this->output
             ]
-    ]);
-
-         "//controller.php?document&upload&patient_id=2635&parent_id=685461&";
-
+        ]);
     }
-
-    protected function convertHtmlToPdf()
-    {
-        $top = $_POST["left_ubmargin"] ?? $GLOBALS['left_ubmargin_default'];
-        $side = $_POST["top_ubmargin"] ?? $GLOBALS['top_ubmargin_default'];
-
-        // convert points to inches-some tricky calculus here! 72 pts/inch
-        $top = round($top / 72.00, 2) . "in";
-        $side = round($side / 72.00, 2) . "in";
-
-        $makePdf = new PdfCreator();
-        $options = array(
-            'margin-top' => $top,
-            'margin-bottom' => '0in',
-            'margin-left' => $side,
-            'margin-right' => $side,
-            'zoom' => '1.045',
-            'print-media-type' => true,
-            'lowquality' => true,
-            'no-outline' => true,
-            'keep-relative-links' => true,
-            'no-images' => false,
-            'grayscale' => false,
-            'page-size' => 'Letter',
-            'orientation' => 'Portrait'
-        );
-
-        return $makePdf->getPdf($this->letter, $options);
-    }
-
-
 }
