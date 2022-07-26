0391d0a00e5a54eee65e5c91acd73a806336b1ed
juggernautsei <sherwingaddis@gmail.com>
2022-07-26 07:29:10 -0400

    List for authorization needed triwest

--- interface/modules/custom_modules/oe-module-prior-authorizations/src/Controller/AuthorizationService.php
+++ interface/modules/custom_modules/oe-module-prior-authorizations/src/Controller/AuthorizationService.php
@@ -182,7 +182,6 @@ class AuthorizationService
         $sql = "SELECT pd.pid AS mrn, pd.fname, pd.lname, mpa.pid, mpa.auth_num, mpa.start_date, mpa.end_date, mpa.cpt, mpa.init_units " . "
             FROM `patient_data` pd " . "
             LEFT JOIN  `module_prior_authorizations` mpa ON pd.pid = mpa.pid " . "
-            LEFT JOIN `transactions` ON `transactions`.`pid` = `pd`.`pid` " . "
             WHERE " . "
             `pd`.`pid` NOT IN ( SELECT `ps`.`pid` FROM `patient_status` ps WHERE `ps`.`status` = 'inactive') " . "
 			ORDER BY pd.lname";
