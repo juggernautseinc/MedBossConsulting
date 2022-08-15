dfc9d6d3b9101f0d550d84194ff105d38c703e38
juggernautsei <sherwingaddis@gmail.com>
2022-08-14 22:59:57 -0400

    Texting schedule out to providers

--- interface/modules/custom_modules/text-messaging-app/public/provider_appt.php
+++ interface/modules/custom_modules/text-messaging-app/public/provider_appt.php
@@ -20,9 +20,10 @@ foreach ($providerArray as $pc_aid) {
     $apptDate = date('Y-m-d', strtotime(' +1 day'));
     $appts = sqlStatement("SELECT pc_title, pc_startTime FROM `openemr_postcalendar_events` " .
         " WHERE pc_aid = ? AND pc_eventDate = ?", [$pc_aid, $apptDate]);
-    $message = '';
+    var_dump($pc_aid);
+    /*$message = '';
     while ($arow = sqlFetchArray($appts)) {
          $message .= $arow['pc_title'] . ", " . $arow['pc_startTime'] . '\r\n';
-    }
+    }*/
 
 }
