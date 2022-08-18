d89695498ac42278683a71ee99f558374cbe59be
juggernautsei <sherwingaddis@gmail.com>
2022-08-18 16:16:59 -0400

    Capture image from phone

--- capture/index.php
+++ capture/index.php
@@ -138,6 +138,34 @@
         }
     }
 
+    function dataURItoBlob( dataURI ) {
+
+        const byteString = atob(dataURI.split(',')[1]);
+        const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        const buffer = new ArrayBuffer(byteString.length);
+        const data = new DataView(buffer);
+
+        for(let i = 0; i < byteString.length; i++ ) {
+
+            data.setUint8( i, byteString.charCodeAt( i ) );
+        }
+
+        return new Blob( [ buffer ], { type: mimeString } );
+    }
+
+    const request = new XMLHttpRequest();
+
+    request.open( "POST", "/upload/url", true );
+
+    const data = new FormData();
+    const dataURI = snapshot.firstChild.getAttribute("src");
+    const imageData = dataURItoBlob(dataURI);
+
+    data.append( "image", imageData, "my_image" );
+
+    request.send( data );
+
 </script>
 </body>
 </html>
