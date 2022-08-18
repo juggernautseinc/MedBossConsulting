dc3674fc49707fb90f74f9615959d812673bc5a7
juggernautsei <sherwingaddis@gmail.com>
2022-08-18 14:08:14 -0400

    Capture image from phone

--- capture/index.php
+++ capture/index.php
@@ -1,121 +1,35 @@
-<?php
-/*
- *  package OpenEMR
- *  link    https://www.open-emr.org
- *  author  Sherwin Gaddis <sherwingaddis@gmail.com>
- *  Copyright (c) 2022.
- *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
- */
-
-?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>Demo - Capture Photo From Webcam Using Javascript</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    <style type="text/css">
-
-        button {
-            width: 120px;
-            padding: 10px;
-            display: block;
-            margin: 20px auto;
-            border: 2px solid #111111;
-            cursor: pointer;
-            background-color: white;
-        }
-
-        #start-camera {
-            margin-top: 50px;
-        }
-
-        #video {
-            display: none;
-            margin: 50px auto 0 auto;
-        }
-
-        #click-photo {
-            display: none;
-        }
-
-        #dataurl-container {
-            display: none;
-        }
-
-        #canvas {
-            display: block;
-            margin: 0 auto 20px auto;
-        }
-
-        #dataurl-header {
-            text-align: center;
-            font-size: 15px;
-        }
-
-        #dataurl {
-            display: block;
-            height: 100px;
-            width: 320px;
-            margin: 10px auto;
-            resize: none;
-            outline: none;
-            border: 1px solid #111111;
-            padding: 5px;
-            font-size: 13px;
-            box-sizing: border-box;
-        }
-
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
-
 <body>
+<div class="display-cover">
+    <video autoplay></video>
+    <canvas class="d-none"></canvas>
 
-<button id="start-camera">Start Camera</button>
-<video id="video" width="320" height="240" autoplay></video>
-<button id="click-photo">Click Photo</button>
-<div id="dataurl-container">
-    <canvas id="canvas" width="320" height="240"></canvas>
-    <div id="dataurl-header">Image Data URL</div>
-    <textarea id="dataurl" readonly></textarea>
+    <div class="video-options">
+        <select name="" id="" class="custom-select">
+            <option value="">Select camera</option>
+        </select>
+    </div>
+
+    <img class="screenshot-image d-none" alt="">
+
+    <div class="controls">
+        <button class="btn btn-danger play" title="Play"><i data-feather="play-circle"></i></button>
+        <button class="btn btn-info pause d-none" title="Pause"><i data-feather="pause"></i></button>
+        <button class="btn btn-outline-success screenshot d-none" title="ScreenShot"><i data-feather="image"></i></button>
+    </div>
 </div>
 
-<script>
-
-    let camera_button = document.querySelector("#start-camera");
-    let video = document.querySelector("#video");
-    let click_button = document.querySelector("#click-photo");
-    let canvas = document.querySelector("#canvas");
-    let dataurl = document.querySelector("#dataurl");
-    let dataurl_container = document.querySelector("#dataurl-container");
-
-    camera_button.addEventListener('click', async function() {
-        let stream = null;
-
-        try {
-            stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        }
-        catch(error) {
-            alert(error.message);
-            return;
-        }
-
-        video.srcObject = stream;
-
-        video.style.display = 'block';
-        camera_button.style.display = 'none';
-        click_button.style.display = 'block';
-    });
-
-    click_button.addEventListener('click', function() {
-        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-        let image_data_url = canvas.toDataURL('image/jpeg');
-
-        dataurl.value = image_data_url;
-        dataurl_container.style.display = 'block';
-    });
-
-</script>
-
+<script src="https://unpkg.com/feather-icons"></script>
+<script src="script.js"></script>
 </body>
 </html>
