d040072b1b41957144a777f2f41bd2bea22e899f
juggernautsei <sherwingaddis@gmail.com>
2022-08-20 09:57:38 -0400

    Capture image from mobile camera

--- capture/photo.php
+++ capture/photo.php
@@ -79,7 +79,7 @@ if (empty($check_source['pid'])) {
 
         #canvas {
             display: block;
-            margin: 0 auto 20px auto;
+            margin: 0 auto 80px auto;
         }
 
         #dataurl-header {
