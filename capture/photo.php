197650768cd547283a57507aa170d63e077b9a8f
juggernautsei <sherwingaddis@gmail.com>
2022-08-25 17:19:59 -0400

    Capture cards via mobile device

--- capture/photo.php
+++ capture/photo.php
@@ -167,7 +167,7 @@ if (empty($check_source['pid'])) {
         canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
         let image_data_url = canvas.toDataURL('image/jpeg');
         dataurl.value = image_data_url;
-        let token = '<?php echo CsrfUtils::collectCsrfToken(); ?>';
+        let token = '<?php echo "thing " . CsrfUtils::collectCsrfToken(); ?>';
 
         let request = new XMLHttpRequest();
         request.open( "POST", "image_receiver.php", true);
