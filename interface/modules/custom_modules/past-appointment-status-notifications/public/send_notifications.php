7b23326a9ff661af9617738df6912d97a33f96bc
juggernautsei <sherwingaddis@gmail.com>
2022-09-15 14:00:50 -0400

    Module Refactoring - building message to email

--- interface/modules/custom_modules/past-appointment-status-notifications/public/send_notifications.php
+++ interface/modules/custom_modules/past-appointment-status-notifications/public/send_notifications.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 /*
  *  package OpenEMR
  *  link    https://www.open-emr.org
@@ -21,29 +22,8 @@ $checkApptStatus = new Notification();
 
 $twodaysago = new DateTime('2 days ago');
 
-var_dump($checkApptStatus->sendList('2022-09-06'));
-//var_dump($checkApptStatus->sendList($twodaysago->format('Y-m-d')));
-die;
-
-
-if (empty($message)) {
-    $message = "No appointments were left in pending status on the calendar ";
-}
-
-$emailSubject = xlt('Pending Appointment Status');
-$email_sender = $GLOBALS['patient_reminder_sender_email'];
-\PHPMailer\PHPMailer\PHPMailer::class;
-$mail->AddReplyTo($email_sender, $email_sender);
-$mail->SetFrom($email_sender, $email_sender);
-$mail->AddAddress($email_sender, $email_sender);
-$mail->Subject = $emailSubject;
-$mail->MsgHTML($message);
-$mail->IsHTML(false);
-$mail->AltBody = $message;
-
-if ($mail->Send()) {
-    file_put_contents('/var/www/html/errors/appt_notification.txt', "Sent " . date('Y-m-d') . "\r\n", FILE_APPEND);
-} else {
-    $mail_status = $mail->ErrorInfo;
-    error_log("EMAIL ERROR: " . errorLogEscape($mail_status), 0);
+try {
+    $checkApptStatus->sendList($twodaysago->format('Y-m-d'));
+} catch (phpmailerException $e) {
+    file_put_contents('/var/www/html/errors/appt_notification_error.txt',  $e->getMessage(), FILE_APPEND);
 }
