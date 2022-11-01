b59b20a47087951c10b06a1fabb0250f8b0b9c24
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-01 15:36:44 -0400

    checking SFTP status

--- library/bill_login_error_reprocess.php
+++ library/bill_login_error_reprocess.php
@@ -41,7 +41,8 @@ function authenticationChecker() :void
     $port = 22;
 
     $client = new X12SFTPClient($host['path'], $user['x12_sftp_login'], $pass);
-    var_dump($client);
+    print_r($client, true);
+
 
 }
 
