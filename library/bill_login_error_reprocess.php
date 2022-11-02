3057a66ba8d4a8a6650666e95faa8029df06556a
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-02 12:32:52 -0400

    reprocess claims

--- library/bill_login_error_reprocess.php
+++ library/bill_login_error_reprocess.php
@@ -8,9 +8,6 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require_once dirname(__FILE__, 2) . '/interface/globals.php';
 
