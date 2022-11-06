d514a40d1179625743bf0b2eb5a25b2e81e61236
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-06 18:40:09 -0500

    refactoring controller location

--- interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs/index.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/lib/canvasjs/index.php
@@ -6,7 +6,7 @@ require_once 'header.php';
 require_once 'sidebar.php';
 require_once 'content.php';
 
-use Juggernaut\App\MonthlyIncomeDataPoints;
+use Juggernaut\App\Controllers\MonthlyIncomeDataPoints;
 
 $genDatapoints = new MonthlyIncomeDataPoints();
 //$insurersId = 106;
