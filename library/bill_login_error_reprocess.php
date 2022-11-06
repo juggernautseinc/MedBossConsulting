70857da672fc93e2703cee8ec5a1a8cea28443d9
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-06 18:08:23 -0500

    reprocess claims

--- library/bill_login_error_reprocess.php
+++ library/bill_login_error_reprocess.php
@@ -29,6 +29,7 @@ function resetClaimStatus() :string
     $client = new X12ClaimRepost($host['path'], $user['x12_sftp_login'], $pass);
 
     if ($client == 'success') {
+        //reset the batches to waiting to be sent when connection comes back online.
         X12ClaimRepost::updateStatus();
         return '<button class="btn-success">' . xlt('Connection to clearinghouse') . '</button>';
     }
