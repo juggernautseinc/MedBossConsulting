c907a48a8d73c23c26a6bf44ee7325e5322ba2e5
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-07 02:34:11 -0500

    Fixing CSV array

--- interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
@@ -48,7 +48,7 @@ $points .= $dataPointsToDisplay;
 <script>
     g = new Dygraph(
         document.getElementById("graphdiv"),
-        <?php echo substr($points, 0, -1); ?>
+        <?php echo substr($points, 0, -2); ?>
     );
 </script>
 </body>
