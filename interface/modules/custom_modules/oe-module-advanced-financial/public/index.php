56c23bf350057ba703d4cd4b5927c03308388ea1
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-06 21:39:04 -0500

    refactoring controller location

--- interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
@@ -23,7 +23,7 @@ use OpenEMR\Core\Header;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo xlt("Documentation Reminders"); ?></title>
+    <title><?php echo xlt("Graphical Income Report"); ?></title>
     <?php Header::setupHeader(['common', 'dygraph'])?>
 </head>
 <body>
