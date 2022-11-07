d00f8e561d0b2e2fddc06795c85d9cc422fddb73
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-07 00:34:27 -0500

    TS - why no array data

--- interface/modules/custom_modules/oe-module-advanced-financial/src/App/Controllers/MonthlyIncomeDataPoints.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/src/App/Controllers/MonthlyIncomeDataPoints.php
@@ -14,6 +14,8 @@ class MonthlyIncomeDataPoints
 {
     private function insuranceIncome($beginningDepositDate, $endingDepositDate, $insurersId): float
     {
+        require_once dirname(__DIR__, 6) . "/globals.php";
+
         $paymentsforthemonth = "SELECT (ar_activity.pay_amount - ar_activity.adj_amount) AS net FROM `ar_activity`
     LEFT JOIN ar_session ON ar_session.session_id = ar_activity.session_id
     WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN ? AND ? AND ar_session.payer_id = ?
