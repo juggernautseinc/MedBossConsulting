2cb59c3d48be7c32d67a2cb919f0f691b5134101
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-07 00:29:56 -0500

    TS - why no array data

--- interface/modules/custom_modules/oe-module-advanced-financial/src/App/Controllers/MonthlyIncomeDataPoints.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/src/App/Controllers/MonthlyIncomeDataPoints.php
@@ -18,7 +18,7 @@ class MonthlyIncomeDataPoints
     LEFT JOIN ar_session ON ar_session.session_id = ar_activity.session_id
     WHERE ar_session.payment_type = 'insurance'  AND ar_session.deposit_date BETWEEN ? AND ? AND ar_session.payer_id = ?
     AND ar_session.deposit_date IS NOT NULL AND ar_activity.deleted IS NULL";
-     echo $beginningDepositDate . ", " . $endingDepositDate;
+     echo $beginningDepositDate . ", " . $endingDepositDate . "<br>";
         $totalpayments = sqlStatement($paymentsforthemonth, [$beginningDepositDate, $endingDepositDate, $insurersId]);
         $u = [];
 
