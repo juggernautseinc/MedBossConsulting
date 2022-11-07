68243eee4bff485a0300910796fd2dc7a5bae220
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-06 21:41:11 -0500

    Starting to use dygraph js

--- interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
+++ interface/modules/custom_modules/oe-module-advanced-financial/public/index.php
@@ -24,7 +24,7 @@ use OpenEMR\Core\Header;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo xlt("Graphical Income Report"); ?></title>
-    <?php Header::setupHeader(['common', 'dygraph'])?>
+    <?php Header::setupHeader(['common', 'dygraphs'])?>
 </head>
 <body>
     <div class="container-lg">
