4fd15ddfa0897c2ad723da292cdd4aa99cbd1f1c
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-08 17:44:51 -0500

    Building a report

--- interface/modules/custom_modules/oe-module-advanced-financial/public/js/report.js
+++ interface/modules/custom_modules/oe-module-advanced-financial/public/js/report.js
@@ -6,7 +6,7 @@
  *  license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-document.getElementById('icompany').addEventListener('change', loadInsuranceCompany());
+document.getElementById('icompany').addEventListener('change', loadInsuranceCompany, false);
 function loadInsuranceCompany() {
 alert('New company selected');
 }
