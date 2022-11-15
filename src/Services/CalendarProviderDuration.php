2aed004d855e272c002c689fd6d0bb210f5b561d
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-15 17:23:02 -0500

    Initial provider duration manager - error 2 can't find template dir

--- src/Services/CalendarProviderDuration.php
+++ src/Services/CalendarProviderDuration.php
@@ -35,6 +35,6 @@ class CalendarProviderDuration
 
     private function getTemplatePath(): string
     {
-        return dirname(__DIR__ . 2) . DIRECTORY_SEPARATOR . "templates/calendar" . DIRECTORY_SEPARATOR;
+        return dirname(__DIR__ . 3) . DIRECTORY_SEPARATOR . "templates/calendar" . DIRECTORY_SEPARATOR;
     }
 }
