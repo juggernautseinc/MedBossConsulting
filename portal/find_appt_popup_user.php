e69225023da92cee3dc07fef3871d65552d4b6ea
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-19 07:19:22 -0500

    Working on output to screen for portal appointments - only 60 min

--- portal/find_appt_popup_user.php
+++ portal/find_appt_popup_user.php
@@ -351,9 +351,10 @@ if ($_REQUEST['providerid']) {
                         }
 
                         $ampmFlag = $ampm;
-                        $providerDuration = \OpenEMR\Services\CalendarProviderDuration::providerDuration($providerid);
+                        (int) $providerDuration = \OpenEMR\Services\CalendarProviderDuration::providerDuration($providerid);
+                        //file_put_contents("/var/www/html/errors/provider.txt", $providerDuration . "\r", FILE_APPEND);
                         $onTheHour = date("g:i", $utime);
-                        if (!str_ends_with($onTheHour, ":00") && $providerDuration = 60) {
+                        if (!str_ends_with($onTheHour, ":00") && $providerDuration == 60) {
                             continue;
                         }
 
