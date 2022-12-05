692a9c352e645e94ed63c2f1f78fbde1efc93e56
juggernautsei <sherwin@affordablecustomehr.com>
2022-12-05 11:56:39 -0500

    Switch insurance from primary to secondary and vice versa

--- interface/patient_file/summary/insurance_change.php
+++ interface/patient_file/summary/insurance_change.php
@@ -16,9 +16,64 @@ use OpenEMR\Services\InsuranceService;
 $patient_insurance = new InsuranceService();
 $companies = new InsuranceCompanyService();
 
-$l = $patient_insurance->getOneByPid($_SESSION['pid'], 'primary');
-$name = $companies->getOneById($l['provider']);
+$primary = $patient_insurance->getOneByPid($_SESSION['pid'], 'primary');
+$pri_name = $companies->getOneById($primary['provider']);
 
-echo "<pre>";
-var_dump($name, $l);
+$secondary = $patient_insurance->getOneByPid($_SESSION['pid'], 'secondary');
+$sec_name = $companies->getOneById($secondary['provider']);
+
+$tertiary = $patient_insurance->getOneByPid($_SESSION['pid'], 'tertiary');
+$tri_name = $companies->getOneById($primary['provider']);
+
 echo "Stage One!";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form id="form" name="theinsuranceform" action="" method="post">
+        <table class="table table-striped">
+            <tr>
+                <td><?php echo xlt('Primary'); ?></td>
+                <td><?php echo xlt($pri_name['name']); ?></td>
+                <td>
+                    <select name="pritype" id="pritype">
+                        <option></option>
+                        <option value="secondary"><?php echo xlt('Secondary') ?></option>
+                        <option value="tertiary"><?php echo xlt('Tertiary') ?></option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><?php echo xlt('Secondary'); ?></td>
+                <td><?php echo xlt($sec_name['name']); ?></td>
+                <td>
+                    <select name="sectype" id="sectype">
+                        <option></option>
+                        <option value="primary"><?php echo xlt('Primary') ?></option>
+                        <option value="tertiary"><?php echo xlt('Tertiary') ?></option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><?php echo xlt('Tertiary'); ?></td>
+                <td><?php echo xlt($tri_name['name']); ?></td>
+                <td>
+                    <select name="tr1type" id="tr1type">
+                        <option></option>
+                        <option value="secondary"><?php echo xlt('Primary') ?></option>
+                        <option value="tertiary"><?php echo xlt('Secondary') ?></option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <input type="">
+    </form>
+</body>
+</html>
