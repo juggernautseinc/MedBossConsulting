c15e98e46fddf10f52f0d7513e14f5789acbf870
juggernautsei <sherwin@affordablecustomehr.com>
2022-12-05 15:06:08 -0500

    Switch insurance from primary to secondary and vice versa

--- interface/patient_file/summary/insurance_change.php
+++ interface/patient_file/summary/insurance_change.php
@@ -15,10 +15,6 @@ use OpenEMR\Services\InsuranceService;
 use OpenEMR\Core\Header;
 use OpenEMR\Common\Csrf\CsrfUtils;
 
-if (!empty($_POST)) {
-    var_dump($_POST['csrf_token_form']);
-    die;
-}
 
 $patient_insurance = new InsuranceService();
 $companies = new InsuranceCompanyService();
