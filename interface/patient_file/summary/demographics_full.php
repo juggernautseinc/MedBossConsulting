dea365476950da25e6443a59c682b9b0b789000c
juggernautsei <sherwin@affordablecustomehr.com>
2022-12-05 10:41:15 -0500

    Switch insurance from primary to secondary and vice versa

--- interface/patient_file/summary/demographics_full.php
+++ interface/patient_file/summary/demographics_full.php
@@ -1055,6 +1055,7 @@ $use_validate_js = $GLOBALS['new_validate'];
 
     document.getElementById('switch').addEventListener('click', changeInsuranceOrder);
     function changeInsuranceOrder() {
+        Event.stopPropagation();
         alert('here');
         // let url = '/interface/patient_file/summary/insurance_change.php'
         // dlgopen(url, '_blank' 500, 500, false, 'Change Insurance Co. order' {
