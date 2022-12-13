6b9385166c75484e8c70697bc3d4fd75fd408db3
juggernautsei <sherwin@affordablecustomehr.com>
2022-12-13 08:16:24 -0500

    Refactoring moving the dispatcher outside the script tags and sending new script tags.

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -1952,13 +1952,13 @@ if ($track_is_registered) {
     if (AclMain::aclCheckCore('admin', 'super') || AclMain::aclCheckCore('acct', 'bill')) {
         $patient_status = sqlQuery("SELECT `status` FROM `patient_status` WHERE `pid` = ? ORDER BY `statusId` DESC LIMIT 1", [$_SESSION['pid']]);
         if ($patient_status['status'] != 'inactive') {
-            ?>
+            ?><script>
             document.getElementById('custompatientnav').innerHTML = '<button class="btn btn-danger" id="addButton">Mark Inactive</button>';
-            <?php
+        </script><?php
         } else {
-            ?>
+            ?><script>
             document.getElementById('custompatientnav').innerHTML = '<button class="btn btn-success" id="addButton">Mark Active</button>';
-            <?php
+            </script><?php
         }
 
         $GLOBALS["kernel"]->getEventDispatcher()->dispatch(RenderEvent::EVENT_RENDER_POST_PAGELOAD, new RenderEvent($pid), 10);
