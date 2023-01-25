c11aa3929272403b647c6135a21427bd757e5daa
juggernautsei <sherwingaddis@gmail.com>
2023-01-25 05:36:02 -0500

    Troublshooting text messages to provider.

--- interface/modules/custom_modules/text-messaging-app/public/provider_appt_notification.php
+++ interface/modules/custom_modules/text-messaging-app/public/provider_appt_notification.php
@@ -37,35 +37,7 @@ while ($prow = sqlFetchArray($providers)) {
 
     if (!empty($number['phonecell'])) {
         $cell = str_replace("-", "", $number['phonecell']);
-        //SendMessage::outBoundMessage($cell, $message);
-    }
-}
-
-
-
-die;
-foreach ($providerArray as $key => $value) {
-    $apptDate = date('Y-m-d', strtotime(' +1 day'));
-    $appts = sqlStatement("SELECT pc_title, pc_startTime FROM `openemr_postcalendar_events` " .
-        " WHERE pc_aid = ? AND pc_eventDate = ? ORDER BY pc_startTime ASC", [$value, $apptDate]);
-    $facility = sqlQuery("SELECT facility FROM `users` WHERE id = ?", [$value]);
-
-    $message = "Your " . $facility['facility'] . " schedule for today: " . $apptDate . "\r\n";
-
-    $mcount = 0;
-    while ($arow = sqlFetchArray($appts)) {
-        $message .= $arow['pc_title'] . ", " . $arow['pc_startTime'] . "\r\n";
-        $mcount++;
-    }
-    if ($mcount == 0) {
-        $message .= "None";
-    }
-    $number = sqlQuery("SELECT phonecell FROM `users` WHERE id = ?", [$value]);
-
-    echo $message . "<br>";
-    if (!empty($number['phonecell'])) {
-        $cell = str_replace("-", "", $number['phonecell']);
-        //SendMessage::outBoundMessage($cell, $message);
+        SendMessage::outBoundMessage($cell, $message);
     }
 }
 
